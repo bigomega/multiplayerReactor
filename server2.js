f2bf6e2ad0e924cb15baa82ddda211949299c861
Bharath Raja <bharathraja@live.com>
2013-01-29 15:04:39 +0530

    flash done

--- server2.js
+++ server2.js
@@ -1,5 +1,9 @@
 var app = require('express').createServer()
 var io = require('socket.io').listen(app);
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var path = require("path");
 
 app.listen(8080);
 
@@ -7,17 +11,76 @@ app.get('/', function (req, res) {
   res.sendfile(__dirname + '/converse2.html');
 });
 
+
+handle={};
+handle["/"] = Gstart;
+handle["/js"] = js;
+handle["/css"] = css;
+function onRequest(request, response) {
+        var pathname  = url.parse(request.url).pathname;
+        var getData = "";
+
+        console.log("Request received for " + pathname);
+        
+        if(url.parse(request.url).query != null)
+            {
+                getData = url.parse(request.url).query;
+                console.log(getData);
+            }
+        
+            request.setEncoding("utf-8");
+            route(handle, pathname, response, getData);
+    }
+
+function route(handle, pathname, response, getData) {
+    console.log("About to route a request for " + pathname);
+    if(typeof(handle[pathname]) === "function")
+        handle[pathname](response, getData);
+    else if(pathname.indexOf(".js")!=-1)
+        handle['/js'](response, pathname);
+    else if(pathname.indexOf(".css")!=-1)
+        handle['/css'](response, pathname);
+    else if(pathname.indexOf(".tpl")!=-1)
+        handle['/tpl'](response, pathname);
+    else { 
+        console.log("No request handler found for " + pathname);
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("404\nPage Not Found");
+        response.end();
+    }
+}
+
+function js(response, fileName) {    
+    response.writeHead(200, {"Content-Type": "text/javascript"});
+    fileName = "." + fileName;
+    response.write(fs.readFileSync(fileName));
+    response.end();
+}
+
+function css(response, fileName) {
+    response.writeHead(200, {"Content-Type": "text/css"});
+    fileName = "." + fileName;
+    response.write(fs.readFileSync(fileName));
+    response.end();
+}
+
+function Gstart(response, fileName){
+    console.log("start")
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(fs.readFileSync("./converse2.html"));
+}
+
 var usernames = [];
 Questions=[{type:2,q:"2+2=4",a:1,qno:0},
 {type:2,q:"3/5>1",a:0,qno:1},
 {type:2,q:"2-12<4",a:1,qno:2},
 {type:2,q:"3*27=71",a:0,qno:3},
-{type:1,time:5,qno:4},
-{type:1,time:4,qno:5},
-{type:1,time:4,qno:6},
-{type:1,time:1,qno:7},
-{type:1,time:2,qno:8},
-{type:1,time:6,qno:9}]
+{type:1,time:2,qno:4},
+{type:1,time:1.45,qno:5},
+{type:1,time:3.8,qno:6},
+{type:1,time:2.5,qno:7},
+{type:1,time:0.8,qno:8},
+{type:1,time:5,qno:9}]
 i=0;
 ans=[]
 s=0;
@@ -40,7 +103,6 @@ io.sockets.on('connection',function(socket){
     io.sockets.emit('res',{score:0,que:Questions[i]});
 
     socket.on('ans',function(data){
-        console.log(socket)
         console.log(data)
         console.log(i+"\n")
         if(i==data.qno){
